ac8497116cc0f13e8aac46d420476068fc00263e
Akasha Sahu <akashasahu2001@gmail.com>
2025-03-11 22:57:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const text = document.getElementById("text");
+
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+const alphaSpan = document.querySelectorAll("span");
+
+alphaSpan.forEach(span => {
+    span.addEventListener("mouseover", () => {
+        span.classList.add("active");
+
+        // Remove the class after animation completes (2s)
+        setTimeout(() => {
+            span.classList.remove("active");
+        }, 5000);
+    });
+});
